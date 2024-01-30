b43d95271f4d1088f9bd33313acc1708685f58f4
Karen Douglas <kdindependence@gmail.com>
2024-01-30 09:48:13 -0600

    Pushing to Heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path');
-const PORT = 3001;
+const port = process.env.PORT || 3001
 const routes = require('./routes/index')
 const apiRoutes = require('./routes/apiRoutes/index')
 
