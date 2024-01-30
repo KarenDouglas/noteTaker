8d948e318542526e35e30a71b63d98462c18d7f2
Karen Douglas <kdindependence@gmail.com>
2024-01-29 20:05:03 -0600

    getting rid of testing

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -2,7 +2,7 @@ const htmlRouter = require('express').Router()
 const path = require('path');
 
 htmlRouter.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '../../public/index.html'))
+  res.status(200).sendFile(path.join(__dirname, '../../public/index.html'))
 );
 
 module.exports = htmlRouter
--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const PORT = 3001;
 
 const app = express();
-const router = require('./routes/index')
+
 app.use(express.static('public'));
 
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
+console.log(`App listening at http://localhost:${PORT}`)
 );
